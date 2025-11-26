efd34724bb73bda40248721ea25de04a74d4eb2c
Mohit kumar <124328700+Mohitkumar1322@users.noreply.github.com>
2025-11-26 20:40:42 +0530

    Add happy_number.cpp (via extension)

---
+++ happy_number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumOfSquareDigits(int n){
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumOfSquareDigits(slow);             // move 1 step
+            fast = sumOfSquareDigits(sumOfSquareDigits(fast)); // move 2 steps
+        } while (slow != fast);
+
+        return slow == 1;  // if meeting point is 1 → happy number
+    }
+};
+
